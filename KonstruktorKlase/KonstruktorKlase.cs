72252ea2565e1f90a53466f3739c069eb71781da
sasviben <sandro.sviben@hotmail.com>
2015-11-12 20:49:01 +0100

    KOnstruktorKlase solved

--- KonstruktorKlase/KonstruktorKlase.cs
+++ KonstruktorKlase/KonstruktorKlase.cs
@@ -7,22 +7,39 @@ namespace Vsite.CSharp
     {
         public int a;
         public string s;
+        public MojaKlasa()
+        {
+            this.a = 10;
+            this.s = "pero";
+        }
+        public MojaKlasa(int a, string s)
+        {
+            this.a = a ;
+            this.s = s;
+        }
     }
 
     class KonstruktorKlase
     {
         static void Main(string[] args)
         {
-            // TODO: Stvoriti objekt klase MojaKlasa i ispisati vrijednosti članova
+            //  Stvoriti objekt klase MojaKlasa i ispisati vrijednosti članova
+
+            MojaKlasa mk = new MojaKlasa();
+            Console.WriteLine(mk.a);
+            Console.WriteLine(mk.s);
+
+            //  U klasu MojaKlasa dodati prazan konstruktor koji inicijalizira svoje članove na neku vrijednost te ponoviti gornji postupak
 
 
+            MojaKlasa mk2 = new MojaKlasa();
+            Console.WriteLine(mk2.a);
+            Console.WriteLine(mk2.s);
 
-            // TODO: U klasu MojaKlasa dodati prazan konstruktor koji inicijalizira svoje članove na neku vrijednost te ponoviti gornji postupak
-
-
-
-            // TODO: U klasi MojaKlasa promijeniti prazan konstruktor tako da prima dva argumenta kojima se inicijaliziraju članovi te ponoviti gornji postupak
-
+            //  U klasi MojaKlasa promijeniti prazan konstruktor tako da prima dva argumenta kojima se inicijaliziraju članovi te ponoviti gornji postupak
+            MojaKlasa mk3 = new MojaKlasa(100, "most");
+            Console.WriteLine(mk3.a);
+            Console.WriteLine(mk3.s);
 
             Console.ReadKey();
         }
